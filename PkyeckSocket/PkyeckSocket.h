65db54aec71bafd1db48f3492fdeee60f60056a9
Hakim <hakim.hauston@nubee.com>
2012-12-03 16:03:00 +0800

    AAPL: added as iOS static library

---
+++ PkyeckSocket/PkyeckSocket.h
@@ -0,0 +1,13 @@
+//
+//  PkyeckSocket.h
+//  PkyeckSocket
+//
+//  Created by Hakim on 3/12/12.
+//  Copyright (c) 2012 Nubee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PkyeckSocket : NSObject
+
+@end
---
+++ PkyeckSocket/PkyeckSocket.m
@@ -0,0 +1,13 @@
+//
+//  PkyeckSocket.m
+//  PkyeckSocket
+//
+//  Created by Hakim on 3/12/12.
+//  Copyright (c) 2012 Nubee. All rights reserved.
+//
+
+#import "PkyeckSocket.h"
+
+@implementation PkyeckSocket
+
+@end
